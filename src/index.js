6e783d5e5a2c116d1daab6be31a18332e2f0a759
VitaliiBaryliuk <vitaliyua912@gmail.com>
2019-03-18 19:06:05 +0200

    changed basename value in index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-  <BrowserRouter basename='/starwarspi-react/'>
+  <BrowserRouter basename='/starwarsapi-react/'>
     <App />
   </BrowserRouter>
   , document.getElementById('root')
