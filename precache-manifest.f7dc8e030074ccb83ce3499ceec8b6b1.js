262867f8408115f7eac6aef353388124e392410e
VitaliiBaryliuk <vitaliyua912@gmail.com>
2019-03-18 18:34:31 +0200

    base build file

---
+++ precache-manifest.f7dc8e030074ccb83ce3499ceec8b6b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d68287afca56996cd380d7e5454810f",
+    "url": "/starwarsapi-react/static/media/logo.4d68287a.png"
+  },
+  {
+    "revision": "af743b6c68bda433892f4230db0769de",
+    "url": "/starwarsapi-react/static/media/background.af743b6c.png"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/starwarsapi-react/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "835a2385317a05ec837d",
+    "url": "/starwarsapi-react/static/js/runtime~main.835a2385.js"
+  },
+  {
+    "revision": "ee14abac02b8dfbaebbe",
+    "url": "/starwarsapi-react/static/js/main.ee14abac.chunk.js"
+  },
+  {
+    "revision": "8475e0c38fdf91f3148b",
+    "url": "/starwarsapi-react/static/js/2.8475e0c3.chunk.js"
+  },
+  {
+    "revision": "ee14abac02b8dfbaebbe",
+    "url": "/starwarsapi-react/static/css/main.fcaa6bb5.chunk.css"
+  },
+  {
+    "revision": "f813260cd5bfb0c509e4153e72ddaa94",
+    "url": "/starwarsapi-react/index.html"
+  }
+];
